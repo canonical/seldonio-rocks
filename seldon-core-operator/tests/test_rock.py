6af4e066782c993959110bfa44ee4abd79bb6b86
Ivan Chvets <ivan.chvets@canonical.com>
2023-05-26 15:22:13 -0400

    feat: updated tests
    Summary of changes:
    - Reorganized tox.ini
    - Updated tests to use filesystem.

--- seldon-core-operator/tests/test_rock.py
+++ seldon-core-operator/tests/test_rock.py
@@ -7,6 +7,7 @@
 
 from pathlib import Path
 
+import os
 import logging
 import pytest
 import subprocess
@@ -25,10 +26,28 @@ def test_rock(ops_test: OpsTest):
     """Test rock."""
     LOCAL_ROCK_IMAGE = read_rock_info()
 
-    # verify that all artifacts are in correct locations
-    subprocess.run(["docker", "run", f"{LOCAL_ROCK_IMAGE}", "exec", "ls", "/opt/seldon-core-operator/manager"], check=True)
-    subprocess.run(["docker", "run", f"{LOCAL_ROCK_IMAGE}", "exec", "ls", "/tmp/operator-resources/service.yaml"], check=True)
-    subprocess.run(["docker", "run", f"{LOCAL_ROCK_IMAGE}", "exec", "ls", "/tmp/operator-resources/configmap.yaml"], check=True)
-    subprocess.run(["docker", "run", f"{LOCAL_ROCK_IMAGE}", "exec", "ls", "/tmp/operator-resources/validate.yaml"], check=True)
-    subprocess.run(["docker", "run", f"{LOCAL_ROCK_IMAGE}", "exec", "ls", "/tmp/operator-resources/crd-v1.yaml"], check=True)
+    # cleanup previous artifacts, if any
+    subprocess.run(["docker", "rm", "rockfs"])
+    subprocess.run(["rm", "-rf", "./rockfs"])
+    subprocess.run(["rm", "-rf", "./rockfs.tar"])
 
+    # create ROCK filesystem
+    subprocess.run(["docker", "create", "--name=rockfs", f"{LOCAL_ROCK_IMAGE}"])
+    subprocess.run(["docker", "export", "rockfs", "--output", "./rockfs.tar"])
+    subprocess.run(["mkdir", "./rockfs"])
+    subprocess.run(["tar", "xvf", "./rockfs.tar", "-C", "rockfs"])
+
+    # verify that all artifacts are in correct locations
+    files = os.listdir('./rockfs/opt/seldon-core-operator/')
+    assert "manager" in files
+
+    files = os.listdir('./rockfs//tmp/operator-resources/')
+    assert "service.yaml" in files
+    assert "configmap.yaml" in files
+    assert "validate.yaml" in files
+    assert "crd-v1.yaml" in files
+
+    # cleanup
+    subprocess.run(["docker", "rm", "rockfs"])
+    subprocess.run(["rm", "-rf", "./rockfs"])
+    subprocess.run(["rm", "-rf", "./rockfs.tar"])
