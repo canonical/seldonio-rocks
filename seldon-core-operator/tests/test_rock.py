861e27304727ee24c27b71b8586e81909d6bcbd5
Ivan Chvets <ivan.chvets@canonical.com>
2023-05-29 09:29:23 -0400

    feat: seldon rock integration
    Summary of changes:
    - Added fixture to test.

--- seldon-core-operator/tests/test_rock.py
+++ seldon-core-operator/tests/test_rock.py
@@ -9,7 +9,9 @@ from pathlib import Path
 
 import os
 import logging
+import random
 import pytest
+import string
 import subprocess
 import yaml
 from pytest_operator.plugin import OpsTest
@@ -21,33 +23,38 @@ def read_rock_info():
     arch = list(ROCKCRAFT["platforms"].keys())[0]
     return f"{name}_{version}_{arch}:{version}"
 
+@pytest.fixture()
+def rock_test_env(tmpdir):
+    """Yields a temporary directory and random docker container name, then cleans them up after."""
+    container_name = "".join([str(i) for i in random.choices(string.ascii_lowercase, k=8)])
+    yield tmpdir, container_name
+
+    try:
+        subprocess.run(["docker", "rm", container_name])
+    except Exception:
+        pass
+    # tmpdir fixture we use here should clean up the other files for us
+
 @pytest.mark.abort_on_fail
-def test_rock(ops_test: OpsTest):
+def test_rock(ops_test: OpsTest, rock_test_env):
     """Test rock."""
+    temp_dir, container_name = rock_test_env
     LOCAL_ROCK_IMAGE = read_rock_info()
 
-    # cleanup previous artifacts, if any
-    subprocess.run(["docker", "rm", "rockfs"])
-    subprocess.run(["rm", "-rf", "./rockfs"])
-    subprocess.run(["rm", "-rf", "./rockfs.tar"])
+    rockfs_tar = temp_dir.join("rockfs.tar")
+    rockfs_dir = temp_dir.mkdir("rockfs")
 
     # create ROCK filesystem
-    subprocess.run(["docker", "create", "--name=rockfs", f"{LOCAL_ROCK_IMAGE}"])
-    subprocess.run(["docker", "export", "rockfs", "--output", "./rockfs.tar"])
-    subprocess.run(["mkdir", "./rockfs"])
-    subprocess.run(["tar", "xvf", "./rockfs.tar", "-C", "rockfs"])
+    subprocess.run(["docker", "create", f"--name={container_name}", f"{LOCAL_ROCK_IMAGE}"])
+    subprocess.run(["docker", "export", f"{container_name}", "--output", rockfs_tar], check=True)
+    subprocess.run(["tar", "xvf", rockfs_tar, "-C", rockfs_dir], check=True)
 
     # verify that all artifacts are in correct locations
-    files = os.listdir('./rockfs/opt/seldon-core-operator/')
+    files = os.listdir(f"{rockfs_dir}/opt/seldon-core-operator/")
     assert "manager" in files
 
-    files = os.listdir('./rockfs//tmp/operator-resources/')
+    files = os.listdir(f"{rockfs_dir}/tmp/operator-resources/")
     assert "service.yaml" in files
     assert "configmap.yaml" in files
     assert "validate.yaml" in files
     assert "crd-v1.yaml" in files
-
-    # cleanup
-    subprocess.run(["docker", "rm", "rockfs"])
-    subprocess.run(["rm", "-rf", "./rockfs"])
-    subprocess.run(["rm", "-rf", "./rockfs.tar"])
